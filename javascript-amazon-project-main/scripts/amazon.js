8e3de087cb53ae9799e44a7bf5ea6ebddf4fb7a6
Jia Sen <jiasen012@gmail.com>
2026-06-11 17:48:06 +0800

    Day 5: Making the main amazon html interactive - loading site with code, making cart item interactable

---
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -0,0 +1,114 @@
+import {cart} from '../data/cart.js';
+
+let productsHTML = '';
+
+function updateCart() {
+      let cartQuantity = 0;
+
+      cart.forEach((product) => {
+        cartQuantity += product.quantity;
+      });
+
+      document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+};
+
+updateCart();
+
+products.forEach((product) => {
+  productsHTML = productsHTML +=`
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        $${(product.priceCents / 100).toFixed(2)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select class="js-quantity-selector-${product.id}">
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart js-added-to-cart-${product.id}">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-name="${product.name}" data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
+
+});
+
+const productsGrid = document.querySelector('.js-products-grid');
+
+productsGrid.innerHTML += productsHTML;
+
+let fadeTimeout;
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+  button.addEventListener('click', () => {
+    const productName = button.dataset.productName;
+    const productId = button.dataset.productId;
+
+    const item = cart.find(item => item.productId === button.dataset.productId);
+    if(item){
+      item.quantity += parseInt(document.querySelector(".js-quantity-selector-" + productId).value);
+    }
+
+    else {
+      cart.push({
+      productName: productName,
+      productId: productId,
+      quantity: parseInt(document.querySelector(".js-quantity-selector-" + productId).value)
+      });
+    }
+
+    updateCart();
+
+    const addedMessage = document.querySelector(".js-added-to-cart-" + productId);
+    addedMessage.classList.add("add-trigger");
+
+    clearTimeout(fadeTimeout);
+
+    fadeTimeout = setTimeout(() => {
+      addedMessage.classList.remove("add-trigger");
+    }, 1000);
+    
+    
+
+    console.log(cart);
+  });
+
+});
+
